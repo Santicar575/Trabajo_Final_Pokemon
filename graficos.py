79333155f14d7878c02736b71db682df22e33ade
Joaquin Castaños Zemborain <128270024+Castanosjoaquin@users.noreply.github.com>
2024-06-07 14:04:59 -0300

    arreglo de branch

--- graficos.py
+++ graficos.py
@@ -94,19 +94,6 @@ def Cant_Pokemons_Epoca_Por_Tipo():
     dic_pokemon = dic_Pokemons()
     types = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
     types_colors = ['#A8A77A', '#EE8130', '#6390F0', '#F7D02C', '#7AC74C', '#96D9D6', '#C22E28', '#A33EA1', '#E2BF65', '#A98FF3', '#F95587', '#A6B91A', '#B6A136', '#735797', '#6F35FC', '#705746', '#B7B7CE', '#D685AD']
-<<<<<<< HEAD
-    equipos = leer_datos_equipos()
-    x, y = types, []
-    llaves = equipos.keys()
-    cant_tipos = {int(llave): {tipo: 0 for tipo in types} for llave in llaves}
-    with open("data/pokemons.csv") as file:
-        for equipo in equipos["0"]:
-            for pokemon in equipo:
-                for tipo in dic_pokemons[pokemon][0]:
-                    cant_tipos[0][tipo] += 1
-                print(dic_pokemons[pokemon][0])
-            
-=======
     equipos,_ = leer_datos_equipos()
     cant_tipos = {llave: {tipo: 0 for tipo in types} for llave in equipos.keys()}
     x, y = [int(epoca) for epoca in equipos.keys()], {}
@@ -115,7 +102,7 @@ def Cant_Pokemons_Epoca_Por_Tipo():
             for pokemon in equipo: #Para cada pokemon en cada equipo (6 veces)
                 for tipo in dic_pokemon[pokemon]: #Para cada tipo en cada pokemon (1/2 veces)
                     if tipo != '': cant_tipos[str(epoca)][tipo] += 1 #Si tiene tipo, lo cuenta
->>>>>>> 51c90d8c8fa7e1a5b8690c21ff783395c3fefec8
+
 
     # Calculate the cumulative sum of pokemon counts for each type
     y = {tipo: [cant_tipos[str(epoca)][tipo] for epoca in x] for tipo in types}
