7800e7d59ace28261c0484aff7e73dbdfa7a63c5
balta2205 <baltasar.oostdijk@gmail.com>
2024-06-07 12:08:59 -0300

    grafico 4/5 empieza

--- graficos.py
+++ graficos.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from Algoritmo_genetico import leer_datos
 
-def leer_datos():
+def leer_epochs():
     epochs = {}
     with open("epochs.csv") as file: 
         lines = file.readlines() 
@@ -13,8 +13,30 @@ def leer_datos():
             epochs[int(line[0])] = [int(line[1]), dic]
     return epochs
 
+def Leer_Datos_Equipos():
+    equipos: dict = {}
+    with open("best_teams.csv") as csv:
+        lines = csv.readlines()
+        for line in lines:
+            line = line.strip().split(",")
+            equipo = line[4:]
+            if line[0] in equipos.keys():
+                equipos[line[0]] += (equipo)
+            else:
+                equipos[line[0]] = [equipo]
+    return equipos
+
+def dic_Pokemons():
+    pokemon_dict = {}
+    with open("data/pokemons.csv") as file: 
+        lines = file.readlines()
+        for line in lines: 
+            line = line.strip().split(',')
+            pokemon_dict[line[1]] = (line[2],line[3])
+    return pokemon_dict
+
 def diversity_vs_epoch():
-    epochs = leer_datos()
+    epochs = leer_epochs()
     x = []
     y = []
     for epoch in epochs: 
@@ -27,7 +49,7 @@ def diversity_vs_epoch():
     plt.show()
 
 def pokemons_in_last_generation():
-    epochs = leer_datos() 
+    epochs = leer_epochs() 
     x = [] 
     y = [] 
     for pokemons in epochs[50][1]: 
@@ -41,4 +63,20 @@ def pokemons_in_last_generation():
     plt.show()
 
 
-       
+def Cant_Pokemons_Epoca_Por_Tipo():
+    dic_pokemons = dic_Pokemons()
+    types = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
+    types_colors = ['#A8A77A', '#EE8130', '#6390F0', '#F7D02C', '#7AC74C', '#96D9D6', '#C22E28', '#A33EA1', '#E2BF65', '#A98FF3', '#F95587', '#A6B91A', '#B6A136', '#735797', '#6F35FC', '#705746', '#B7B7CE', '#D685AD']
+    equipos = Leer_Datos_Equipos()
+    x, y = types, []
+    llaves = equipos.keys()
+    cant_tipos = {int(llave): {tipo: 0 for tipo in types} for llave in llaves}
+    with open("data/pokemons.csv") as file:
+        for equipo in equipos["0"]:
+            for pokemon in equipo:
+                for tipo in dic_pokemons[pokemon][0]:
+                    cant_tipos[0][tipo] += 1
+                print(dic_pokemons[pokemon][0])
+            
+
+Cant_Pokemons_Epoca_Por_Tipo()
